61a2d255cba6b297ef0f29e0b0d92880630b4ad1
toastynerd <tylermorgan86@gmail.com>
2021-07-18 22:57:01 -0700

    basic version of settings form

--- app/javascript/components/SettingsForm.jsx
+++ app/javascript/components/SettingsForm.jsx
@@ -2,33 +2,60 @@ import React, { useState } from 'react';
 import parseINIString from '../parsers/ini_parser';
 
 const SettingsForm = () => {
-  const [settings, setSettings] = useState({});
+  const [formData, setFormData] = useState({
+    settings: {},
+    rfid: "",
+    printFile: "",
+    notes: ""
+  });
 
-  const onFileChange = (event) => {
+  const handleFileChange = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = (theFile) => {
       const parsed = parseINIString(theFile.target.result);
-      setSettings(parsed);
+      setFormData(prevFormData => {
+        return {
+          ...formData,
+          settings: parsed
+        };
+      });
     };
     reader.readAsText(file);
   };
 
   const handleChange = (event) => {
-    setSettings(event.target.value);
+    setFormData(prevFormData => {
+      return {
+        ...formData,
+        [event.target.name]: event.target.value
+      };
+    });
   };
 
   return (
     <form>
       <label htmlFor="settings-upload">Settings</label>
-      {Object.keys(settings).map(category => {
+      <input type="file" onChange={handleFileChange} />
+
+      <label htmlFor="rfid">RFID</label>
+      <input type="text" name="rfid" value={formData.rfid} onChange={handleChange} />
+
+      <label htmlFor="print-file">Print File</label>
+      <input type="text" name="printFile" value={formData.printFile} onChange={handleChange} />
+
+      <label htmlFor="notes">Notes</label>
+      <textarea name="notes" value={formData.notes} onChange={handleChange}/>
+
+      <button type="submit">Submit Print Data</button>
+
+      {Object.keys(formData.settings).map(category => {
         return (
-          <div>
-            <label htmlFor={category}>{category}</label>
-            <input type="text" value={category} onChange={handleChange}/>
+          <div key={category}>
+            <h2>{ category }</h2>
             <ul>
-              {Object.keys(settings[category]).map(settingName => {
-                return <li>{settingName}: {settings[category][settingName]}</li>
+              {Object.keys(formData.settings[category]).map(settingName => {
+                return <li key={settingName}>{settingName}: {formData.settings[category][settingName]}</li>
               })}
             </ul>
           </div>
